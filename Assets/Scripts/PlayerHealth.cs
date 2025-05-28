5289fe921e531d9bea3dbc24ad7ba8c377ca1429
Steven Duong <stevenduongx@gmail.com>
2025-05-28 00:04:48 -0600

    create: player health bar ui

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth, maxHealth, damageAmount;
+    public HealthBar healthBar;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(currentHealth);
     }
 
     // Update is called once per frame
@@ -18,6 +20,7 @@ public class PlayerHealth : MonoBehaviour
 
     public void DealDamage() {
         currentHealth -= damageAmount;
+        healthBar.SetHealth(currentHealth);
         if (currentHealth <= 0) {
             gameObject.SetActive(false);
         }
