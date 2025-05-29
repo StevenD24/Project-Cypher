6fb77f71dc76c47ae049473e2bbd1537d8c0057f
Steven Duong <stevenduongx@gmail.com>
2025-05-28 23:33:30 -0600

    add: coin score and effects

--- Assets/Scripts/Pickups.cs
+++ Assets/Scripts/Pickups.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Pickups : MonoBehaviour
 {
     public int coinScore = 0;
+    public GameObject cfxr3HitLightEffect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() { }
@@ -13,9 +14,26 @@ public class Pickups : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Coin")
-        {   
+        {
             coinScore++;
-            Destroy(collision.gameObject);
+            Debug.Log("Coins collected: " + coinScore);
+
+            // Play CFXR3 Hit Light B effect at Player's position
+            if (cfxr3HitLightEffect != null)
+            {
+                GameObject effectInstance = Instantiate(
+                    cfxr3HitLightEffect,
+                    transform.position, // Player's position
+                    Quaternion.identity
+                );
+
+                // Make the effect follow the player by making it a child
+                effectInstance.transform.SetParent(transform);
+
+                Destroy(effectInstance, 2f); // Destroy after 2 seconds
+            }
+
+            Destroy(collision.gameObject); // Destroy the coin
         }
     }
 }
